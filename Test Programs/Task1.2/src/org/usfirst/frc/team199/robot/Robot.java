140e59165e6b9b698f883255c0daf754bc88d4e0
Laura McGann <hailey.mcgann@gmail.com>
2016-10-13 15:53:43 -0700

    tests encoder and PID
    1) Tests encoder is outputting values
    2) sets encoder ratio
    3) puts PID constant values in SmartDashboard
    4) tests PID turning and driving forward

---
+++ Test Programs/Task1.2/src/org/usfirst/frc/team199/robot/Robot.java
@@ -0,0 +1,211 @@
+
+package org.usfirst.frc.team199.robot;
+
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+    final String defaultAuto = "Default";
+    final String customAuto = "My Auto";
+    String autoSelected;
+    SendableChooser chooser;
+	
+    
+    private static Encoder rightEncoder;
+    private static Encoder leftEncoder;
+    //private static SpeedController rightSpeed;
+    //private static SpeedController leftSpeed;
+    private static Talon rightMotor;
+    private static Talon leftMotor;
+    private static RobotDrive robotDrive = new RobotDrive(leftMotor, rightMotor);
+    private static Joystick rightJoy = new Joystick(1);
+    private static Joystick leftJoy = new Joystick(2);
+    private static AnalogGyro gyro = new AnalogGyro(3);
+    
+    private double totalDistance;
+    private double totalDegrees;
+    private double kpDistance;
+    private double kpDegrees;
+    private double kdDistance;
+    private double kdDegrees;
+    private double kiDistance;
+    private double kiDegrees;
+    
+    /**
+     * Updates the value of the encoder in SmartDashboard
+     * to the current average of the left and right encoder values
+     * */
+    public void update(){
+    	SmartDashboard.putNumber("encoder", getEncoder());
+    }
+    
+    /**
+     * @return the ratio of distance traveled to the encoder's value
+     * */
+    public double setEncoderRatio(double distanceTraveled){
+    	return  distanceTraveled/SmartDashboard.getNumber("encoder");
+    }
+    
+    /**
+     * PID driving forward/backward
+     * */
+    public void autoDrive(double speed){
+    	robotDrive.tankDrive(speed, speed);
+    }
+    
+    /**
+     * PID turning
+     * */
+    public void autoTurn(double angle){
+    	int sign = (int) (angle/Math.abs(angle));
+    	robotDrive.tankDrive(sign, -sign);	
+    }
+    
+    /**
+     * @return the value of the gyroscope
+     * */
+    public double getGyro(){
+    	return gyro.getAngle();
+    }
+    
+    /**
+     * @return the average of the left and right encoder values
+     * */
+    public double getEncoder(){
+    	return (leftEncoder.get() + rightEncoder.get())/2;
+    }
+    
+    /**
+     * Resets variables totalDistance and totalDegrees to zero
+     * */
+    public void resetTotals() {
+    	totalDistance = 0;
+    	totalDegrees = 0;
+    }
+    
+    /**
+     * @param target target distance you want the robot to travel
+     * @return necessary motor speed
+     * */
+    public double getMotorOutputEncoder(double target){
+    	double error = target - getEncoder();
+    	double traveled = getEncoder();
+    	double speed = (leftEncoder.getRate() + rightEncoder.getRate())/2;
+    	totalDistance += traveled;
+    	return error*kpDistance - speed*kdDistance + totalDistance*kiDistance;
+    }
+    
+    /**
+     * @param target target angle you want the robot to turn (in degrees)
+     * @return necessary motor speed
+     * */
+    public double getMotorOutputGyro(double target){
+    	double error = target - getGyro();
+    	double traveled = getGyro();
+    	double speed = gyro.getRate();
+    	totalDegrees += traveled;
+    	return error*kpDegrees - speed*kdDegrees + totalDegrees*kiDegrees;
+    }
+    
+    /**
+     * Sets the constants used for PID control to their values in SmartDashboard
+     * */
+    public void setConstants(){
+    	kpDistance = SmartDashboard.getNumber("kp Distance");
+        kpDegrees = SmartDashboard.getNumber("kp Degrees");
+        kdDistance = SmartDashboard.getNumber("kd Distance");
+        kdDegrees = SmartDashboard.getNumber("kd Degrees");
+        kiDistance = SmartDashboard.getNumber("ki Distance");
+        kiDegrees = SmartDashboard.getNumber("ki Degrees");
+    }
+    
+    
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+        chooser = new SendableChooser();
+        chooser.addDefault("Default Auto", defaultAuto);
+        chooser.addObject("My Auto", customAuto);
+        SmartDashboard.putData("Auto choices", chooser);
+        
+        SmartDashboard.putNumber("kp Distance", 0);
+        SmartDashboard.putNumber("kp Degrees", 0);
+        SmartDashboard.putNumber("kd Distance", 0);
+        SmartDashboard.putNumber("kd Degrees", 0);
+        SmartDashboard.putNumber("ki Distance", 0);
+        SmartDashboard.putNumber("ki Degrees", 0);
+        
+        SmartDashboard.putNumber("target angle", 0);
+        SmartDashboard.putNumber("target distance", 0);
+        
+    }
+    
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
+	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
+	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
+	 * below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
+	 * If using the SendableChooser make sure to add them to the chooser code above as well.
+	 */
+    public void autonomousInit() {
+    	autoSelected = (String) chooser.getSelected();
+//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
+		System.out.println("Auto selected: " + autoSelected);
+		setConstants();
+		resetTotals();
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    	switch(autoSelected) {
+    	case customAuto:
+        //Put custom auto code here
+            autoTurn(getMotorOutputGyro(SmartDashboard.getNumber("target angle")));
+            break;
+    	case defaultAuto:
+    	default:
+    	//Put default auto code here
+    		autoDrive(getMotorOutputEncoder(SmartDashboard.getNumber("target distance")));
+            break;
+    	}
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+    	robotDrive.tankDrive(leftJoy.getThrottle(), rightJoy.getThrottle());
+    	update();
+    	SmartDashboard.putNumber("encoder ratio", setEncoderRatio(12));
+        
+    }
+    
+    /**
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    
+    }
+    
+}
